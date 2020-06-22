d99e05e3ebeff4d0b683f163f7d982ec71d26066
Rattan-ArthTexh <rattan.arthtech.s@gmail.com>
2020-06-22 11:54:04 +0530

    subBranch commit

---
+++ MapDemo/SubBrach/SubBranchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SubBranchViewController.swift
+//  MapDemo
+//
+//  Created by SAT PAL on 22/06/20.
+//  Copyright © 2020 SAT PAL. All rights reserved.
+//
+
+import UIKit
+
+class SubBranchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("sub branch")
+    }
+    
+
+
+}
